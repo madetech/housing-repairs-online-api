0c6d5e7bbf7b06d5be4fd9eb1dfc3e5d1a1442b3
maysakanoni <maysa@madetech.com>
2021-09-28 13:31:55 +0100

    added an editorconfig

---
+++ housing-repairs-online-api.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace housing_repairs_online_api.Tests.Controllers
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
