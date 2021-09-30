5bcbf2257603df3b11d4090c207400e344971439
Bhavesh-Patel <Bhavesh-Patel@users.noreply.github.com>
2021-09-30 11:05:23 +0100

    Update WeatherForecastController.cs
    Added root route

--- HousingRepairsOnlineApi/Controllers/WeatherForecastController.cs
+++ HousingRepairsOnlineApi/Controllers/WeatherForecastController.cs
@@ -24,6 +24,7 @@ namespace HousingRepairsOnlineApi.Controllers
         }
 
         [HttpGet]
+        [Route("/")]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
