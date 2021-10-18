480568f7a3d3355d730f6ff84b0c3492763b54e4
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-10-18 13:32:50 +0100

    Created AddressGateway and tests for it
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: elena-vi <elena@madetech.com>

---
+++ HousingRepairsOnlineApi.Tests/GatewaysTests/AddressGatewayTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using HousingRepairsOnlineApi.Gateways;
+using Moq;
+using Xunit;
+
+namespace HousingRepairsOnlineApi.Tests.GatewaysTests
+{
+    public class AddressGatewayTests
+    {
+        private readonly AddressGateway addressGateway;
+        private readonly Mock<HttpClient> httpClientMock;
+
+        public AddressGatewayTests()
+        {
+            httpClientMock = new Mock<HttpClient>();
+
+            addressGateway = new AddressGateway(httpClientMock.Object);
+        }
+
+        [Fact]
+        public void ARequestIsMade()
+        {
+            // Arrange
+            const string Postcode = "M3 OW";
+            var expectedRequest = new HttpRequestMessage(HttpMethod.Get,
+                "https://api.github.com/repos/dotnet/AspNetCore.Docs/branches");
+
+            // Act
+            _ = addressGateway.Search(Postcode);
+
+            // Assert
+            httpClientMock.Verify(x => x.SendAsync(expectedRequest), Times.Once);
+        }
+
+    }
+}
---
+++ HousingRepairsOnlineApi/Gateways/AddressGateway.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace HousingRepairsOnlineApi.Gateways
+{
+    public class AddressGateway : IAddressGateway
+    {
+        private HttpClient httpClient;
+        public AddressGateway(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public IEnumerable<object> Search(string postcode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
