15c62e105b4cb8435b21d4bcaf3515ea063a3705
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-10-27 15:36:58 +0100

    Removed file config reading from SoREngine
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- HousingRepairsOnlineApi.Tests/HelpersTests/SoREngineTests.cs
+++ HousingRepairsOnlineApi.Tests/HelpersTests/SoREngineTests.cs
@@ -1,56 +1,64 @@
-﻿using HousingRepairsOnlineApi.Helpers;
+﻿using System.Collections.Generic;
+using HousingRepairsOnlineApi.Helpers;
 using Xunit;
 
 namespace HousingRepairsOnlineApi.Tests.HelpersTests
 {
     public class SoREngineTests
     {
+        private SoREngine systemUnderTest;
+
+        public SoREngineTests()
+        {
+            IDictionary<string, IDictionary<string, IDictionary<string, string>>> mapping = new Dictionary<string, IDictionary<string, IDictionary<string, string>>>()
+            {
+                {
+                    "kitchen",
+                    new Dictionary<string, IDictionary<string, string>>
+                    {
+                        {
+                            "cupboards", new Dictionary<string, string>
+                            {
+                                { "doorHangingOff", "N373049" },
+                                { "doorMissing", "N373049" },
+                            }
+                        },
+                        {
+                            "worktop", new Dictionary<string, string>
+                            {
+                                { "damaged", "N372005" },
+                            }
+                        },
+                    }
+                },
+                {
+                    "bathroom",
+                    new Dictionary<string, IDictionary<string, string>>
+                    {
+                        {
+                            "bath", new Dictionary<string, string>
+                            {
+                                {"bathTaps", "N631301"},
+
+                            }
+                        },
+                    }
+                }
+            };
+            systemUnderTest = new SoREngine(mapping);
+        }
+
         [Theory]
         [InlineData("kitchen", "cupboards", "doorHangingOff", "N373049")]
         [InlineData("kitchen", "cupboards", "doorMissing", "N373049")]
         [InlineData("kitchen", "worktop", "damaged", "N372005")]
-        [InlineData("kitchen", "dampMould", "dampMould", "N114001")]
-        [InlineData("kitchen", "electrical", "extractorFan", "N841007")]
-        [InlineData("kitchen", "electrical", "sockets", "N861511")]
-        [InlineData("kitchen", "electrical", "lightFitting", "N858111")]
-        //[InlineData("kitchen", "electrical", "cookerSwitch", "TBC-cookerSwitch")]
-        [InlineData("kitchen", "sink", "taps", "N630147")]
-        [InlineData("kitchen", "sink", "pipeworkLeak", "N630147")]
-        //[InlineData("kitchen", "sink", "damage", "TBC-sinkDamage")]
-        [InlineData("kitchen", "wallsFloorsCeiling", "wallTiles", "N431041")]
-        [InlineData("kitchen", "wallsFloorsCeiling", "floorTiles", "N432005")]
-        [InlineData("kitchen", "wallsFloorsCeiling", "lightFitting", "N858111")]
-        [InlineData("kitchen", "wallsFloorsCeiling", "skirtingBoardArchitrave", "N381001")]
-        [InlineData("kitchen", "wallsFloorsCeiling", "plasteringCeiling", "N413305")]
-        [InlineData("kitchen", "wallsFloorsCeiling", "plasteringWalls", "N431023")]
-        [InlineData("kitchen", "window", "stuckShut", "N318125")]
-        [InlineData("kitchen", "window", "condensation", "N318151")]
-        [InlineData("kitchen", "door", "backDoorWooden", "N324123")]
-        [InlineData("kitchen", "door", "backDoorUPVC", "N325117")]
-        [InlineData("kitchen", "door", "backFrenchDoors", "N325117")]
-        [InlineData("kitchen", "door", "internal", "N330007")]
-        [InlineData("kitchen", "door", "sliding", "N330007")]
         [InlineData("bathroom", "bath", "bathTaps", "N631301")]
-        [InlineData("bathroom", "bath", "sealAroundBath", "N630945")]
-        [InlineData("bathroom", "bath", "bathPanel", "N630945")]
-        [InlineData("bathroom", "bath", "bathBlockage", "N630945")]
-        [InlineData("bathroom", "shower", "electricShower", "N631131")]
-        [InlineData("bathroom", "shower", "showerTap", "N631337")]
-        [InlineData("bathroom", "shower", "showerHose", "N631111")]
-        [InlineData("bathroom", "shower", "showerHead", "N631121")]
-        [InlineData("bathroom", "sink", "sinkTaps", "N631301")]
-        [InlineData("bathroom", "sink", "damagedSink", "N630714")]
-        [InlineData("bathroom", "sink", "leakBlockedSink", "N620507")]
-        [InlineData("bathroom", "toilet", "notFlushing", "N630573")]
-        [InlineData("bathroom", "toilet", "overflowing", "N630573")]
-        [InlineData("bathroom", "toilet", "looseFromFloor", "N630516")]
         public void GivenLocationProblemIssue_WhenCallingMapSorCode_ThenExpectedSorIsReturned(string location, string problem, string issue, string expectedSor)
         {
             // Arrange
-            var sut = new SoREngine();
 
             // Act
-            var actual = sut.MapSorCode(location, problem, issue);
+            var actual = systemUnderTest.MapSorCode(location, problem, issue);
 
             // Assert
             Assert.Equal(expectedSor, actual);
--- HousingRepairsOnlineApi/Helpers/SoREngine.cs
+++ HousingRepairsOnlineApi/Helpers/SoREngine.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
 
 namespace HousingRepairsOnlineApi.Helpers
 {
     public class SoREngine
     {
+        private readonly IDictionary<string, IDictionary<string, IDictionary<string, string>>> soRMapping;
+
+        public SoREngine(IDictionary<string, IDictionary<string, IDictionary<string, string>>> soRMapping)
+        {
+            this.soRMapping = soRMapping;
+        }
+
         public string MapSorCode(string location, string problem, string issue)
         {
-            var reader = new StreamReader("SoRConfig.json");
-            var json = reader.ReadToEnd();
-            var soRMapping = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>(json);
             var result = soRMapping[location][problem][issue];
 
             return result;
