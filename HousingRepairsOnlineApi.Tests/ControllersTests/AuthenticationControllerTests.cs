77c2cefdbb1168b45d797e0ac8772cc8963a6db2
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-11-04 21:59:06 +0000

    Created authentication controller

---
+++ HousingRepairsOnlineApi.Tests/ControllersTests/AuthenticationControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using HousingRepairsOnlineApi.Controllers;
+using Xunit;
+
+namespace HousingRepairsOnlineApi.Tests.ControllersTests
+{
+    public class AuthenticationControllerTests : ControllerTests
+    {
+        private AuthenticationController systemUnderTest;
+
+        public AuthenticationControllerTests()
+        {
+            systemUnderTest = new AuthenticationController(null);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("   ")]
+        public async Task GivenAnInvalidIdentifier_WhenAuthenticateIsCalled_ThenUnauthorisedResponseIsGiven(string identifier)
+        {
+            var result = await systemUnderTest.Authenticate(identifier);
+
+            GetStatusCode(result).Should().Be((int)HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task GivenAValidIdentifierThatIsNotAuthorised_WhenAuthenticateIsCalled_ThenUnauthorisedResponseIsGiven()
+        {
+            const string identifier = "M3";
+            var result = await systemUnderTest.Authenticate(identifier);
+
+            GetStatusCode(result).Should().Be((int)HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task GivenAValidIdentifierThatIsAuthorised_WhenAuthenticateIsCalled_ThenOkResponseIsGiven()
+        {
+            const string identifier = "M3";
+            systemUnderTest = new AuthenticationController(identifier);
+            var result = await systemUnderTest.Authenticate(identifier);
+
+            GetStatusCode(result).Should().Be((int)HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GivenAValidIdentifierThatIsAuthorised_WhenAuthenticateIsCalled_ThenResponseContainsANonNullAndNonEmptyString()
+        {
+            const string identifier = "M3";
+            systemUnderTest = new AuthenticationController(identifier);
+            var result = await systemUnderTest.Authenticate(identifier);
+
+            GetResultData<string>(result).Should().NotBeNull().And.NotBeEmpty();
+        }
+    }
+}
---
+++ HousingRepairsOnlineApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingRepairsOnlineApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly string identifier;
+
+        public AuthenticationController(string identifier)
+        {
+            this.identifier = identifier;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Authenticate(string identifier)
+        {
+            IActionResult result;
+            if (string.IsNullOrWhiteSpace(identifier) || this.identifier != identifier)
+            {
+                result = Unauthorized();
+            }
+            else
+            {
+                result =  Ok("token");
+            }
+
+            return await Task.FromResult(result);
+        }
+    }
+}
