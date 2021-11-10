4bb73d383d52400fc84d1bf3243f659cd501212c
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-11-10 12:22:27 +0000

    Fixed tests

--- HousingRepairsOnlineApi.Tests/GatewaysTests/AddressGatewayTests.cs
+++ HousingRepairsOnlineApi.Tests/GatewaysTests/AddressGatewayTests.cs
@@ -11,6 +11,9 @@ namespace HousingRepairsOnlineApi.Tests.GatewaysTests
 {
     public class AddressGatewayTests
     {
+        private const string authenticationIdentifier = "super secret";
+        private const string Token = "token";
+        private const string AddressApiEndpoint = "https://our-porxy-UH.api";
         private readonly AddressGateway addressGateway;
         private readonly Mock<HttpClient> httpClientMock;
         private readonly MockHttpMessageHandler mockHttp;
@@ -18,21 +21,25 @@ namespace HousingRepairsOnlineApi.Tests.GatewaysTests
         public AddressGatewayTests()
         {
             mockHttp = new MockHttpMessageHandler();
-            const string addressApiEndpoint = "https://our-porxy-UH.api";
-            const string addressApiKey = "super secret";
-            addressGateway = new AddressGateway(mockHttp.ToHttpClient(), addressApiEndpoint, addressApiKey);
+
+            mockHttp.Expect($"{AddressApiEndpoint}/authentication?identifier={authenticationIdentifier}")
+                .Respond(HttpStatusCode.OK, x =>new StringContent(Token));
+
+            addressGateway = new AddressGateway(mockHttp.ToHttpClient(), AddressApiEndpoint, authenticationIdentifier);
         }
 
         [Fact]
-        public void ARequestIsMade()
+        public async void ARequestIsMade()
         {
             // Arrange
             const string Postcode = "M3 OW";
 
-            mockHttp.Expect($"https://our-porxy-UH.api/addresses?postcode={Postcode}")
-                .WithHeaders("X-API-Key", "super secret");
+            mockHttp.Expect($"{AddressApiEndpoint}/addresses?postcode={Postcode}")
+                .WithHeaders("Authorization", $"Bearer {Token}")
+                .Respond(HttpStatusCode.OK, x=> new StringContent("[]"));
+
             // Act
-            _ = addressGateway.Search(Postcode);
+            _ = await addressGateway.Search(Postcode);
 
             // Assert
             mockHttp.VerifyNoOutstandingExpectation();
@@ -44,8 +51,8 @@ namespace HousingRepairsOnlineApi.Tests.GatewaysTests
             // Arrange
             const string Postcode = "M3 0W";
 
-            mockHttp.Expect($"https://our-porxy-UH.api/addresses?postcode={Postcode}")
-                .WithHeaders("X-API-Key", "super secret")
+            mockHttp.Expect($"{AddressApiEndpoint}/addresses?postcode={Postcode}")
+                .WithHeaders("Authorization", $"Bearer {Token}")
                 .Respond("application/json",
                     "[{ \"UPRN\": \"944225244413\", " +
                     "\"Postbox\": \"null\", " +
@@ -76,8 +83,8 @@ namespace HousingRepairsOnlineApi.Tests.GatewaysTests
             // Arrange
             const string Postcode = "M3 0W";
 
-            mockHttp.Expect($"https://our-porxy-UH.api/addresses?postcode={Postcode}")
-                .WithHeaders("X-API-Key", "super secret")
+            mockHttp.Expect($"{AddressApiEndpoint}/addresses?postcode={Postcode}")
+                .WithHeaders("Authorization", $"Bearer {Token}")
                 .Respond(statusCode: (HttpStatusCode)503);
             // Act
             var data = await addressGateway.Search(Postcode);
