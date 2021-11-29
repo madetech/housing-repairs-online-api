b2e7a68b04a7fa6c28c4d468c303ef93f1ee59bb
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-11-29 15:42:14 +0000

    Created Appointments controller with AvailableAppointments endpoint

---
+++ HousingRepairsOnlineApi.Tests/ControllersTests/AppointmentsControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using HousingRepairsOnlineApi.Controllers;
+using Xunit;
+
+namespace HousingRepairsOnlineApi.Tests.ControllersTests
+{
+    public class AppointmentsControllerTests : ControllerTests
+    {
+        private AppointmentsController sytemUndertest;
+
+        public AppointmentsControllerTests()
+        {
+            sytemUndertest = new AppointmentsController();
+        }
+
+        [Fact]
+        public async Task TestEndpoint()
+        {
+            const string RepairCode = "N98765";
+            const string Uprn = "12345";
+            var result = await sytemUndertest.AvailableAppointments(RepairCode, Uprn);
+
+            GetStatusCode(result).Should().Be(200);
+        }
+    }
+}
---
+++ HousingRepairsOnlineApi/Controllers/AppointmentsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingRepairsOnlineApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> AvailableAppointments([FromQuery] string repairCode, [FromQuery] string uprn)
+        {
+            return Ok();
+        }
+    }
+}
