4a907cb021e6d1cfde1512753366d3edb0408f52
Bhavesh Patel <bhavesh.patel@madetech.com>
2021-11-29 15:50:29 +0000

    Created RetrieveAvailableAppointmentsUseCase

---
+++ HousingRepairsOnlineApi.Tests/UseCasesTests/RetrieveAvailableAppointmentsUseCaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using HousingRepairsOnlineApi.UseCases;
+using Xunit;
+
+namespace HousingRepairsOnlineApi.Tests.UseCasesTests
+{
+    public class RetrieveAvailableAppointmentsUseCaseTests
+    {
+        [Fact]
+        public async void GivenNullRepairCode_WhenExecute_ThenArgumentNullExceptionIsThrown()
+        {
+            // Arrange
+            var systemUnderTest = new RetrieveAvailableAppointmentsUseCase();
+
+            // Act
+            Func<Task> act = async () => await systemUnderTest.Execute(null, "A UPRN");
+
+            // Assert
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+    }
+}
---
+++ HousingRepairsOnlineApi/UseCases/RetrieveAvailableAppointmentsUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HousingRepairsOnlineApi.UseCases
+{
+    public class RetrieveAvailableAppointmentsUseCase
+    {
+        public async Task Execute(string repairCode, string uprn)
+        {
+            throw new ArgumentNullException();
+        }
+    }
+}
