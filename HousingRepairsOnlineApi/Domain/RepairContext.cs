818526260b863661cb918d0f83dbb5ed51b507b2
Olly Swanson <olly.swanson95@gmail.com>
2022-09-06 15:50:31 +0100

    Connect to Postgres

---
+++ HousingRepairsOnlineApi/Domain/RepairContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HousingRepairsOnlineApi.Domain;
+
+public class RepairContext : DbContext
+{
+    public RepairContext(DbContextOptions options) : base(options) {}
+
+    public DbSet<Repair> Repairs { get; set; }
+
+    // TODO: Do we really want to create the tables like this???
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Repair>().ToTable(nameof(Repair));
+        }
+}
