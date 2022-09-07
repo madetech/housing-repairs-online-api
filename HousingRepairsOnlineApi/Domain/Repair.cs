858354888472d0683fd105f2cbac9aed98ef469f
jamiejordanmt <jamie.jordan@madetech.com>
2022-09-07 14:18:13 +0100

    WIP: Adds jsonb annotations to seed repairs table and starts postgres gateway

--- HousingRepairsOnlineApi/Domain/Repair.cs
+++ HousingRepairsOnlineApi/Domain/Repair.cs
@@ -1,20 +1,33 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HousingRepairsOnlineApi.Domain
 {
     public class Repair
     {
-        [JsonProperty("id")]
         public string Id { get; set; }
         public string Postcode { get; set; }
         public string SOR { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairAddress Address { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairLocation Location { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairProblem Problem { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairIssue Issue { get; set; }
         public string ContactPersonNumber { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairDescription Description { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairContactDetails ContactDetails { get; set; }
+
+        [Column(TypeName = "jsonb")]
         public RepairAvailability Time { get; set; }
     }
 }
