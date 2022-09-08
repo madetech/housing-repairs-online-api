161288fb939b22aec428df3db45405968f6de223
Olly Swanson <olly.swanson95@gmail.com>
2022-09-08 14:06:20 +0100

    Rename `cosmosGateway` to `repairStorageGateway`
    The purpose of using an interface is so that the code can be abstract
    over the implementation...

--- HousingRepairsOnlineApi/UseCases/SaveRepairRequestUseCase.cs
+++ HousingRepairsOnlineApi/UseCases/SaveRepairRequestUseCase.cs
@@ -9,14 +9,14 @@ namespace HousingRepairsOnlineApi.UseCases
 {
     public class SaveRepairRequestUseCase : ISaveRepairRequestUseCase
     {
-        private readonly IRepairStorageGateway cosmosGateway;
+        private readonly IRepairStorageGateway repairStorageGateway;
         private readonly IBlobStorageGateway storageGateway;
         private readonly ISoREngine sorEngine;
 
-        public SaveRepairRequestUseCase(IRepairStorageGateway cosmosGateway, IBlobStorageGateway storageGateway, ISoREngine sorEngine)
+        public SaveRepairRequestUseCase(IRepairStorageGateway repairStorageGateway, IBlobStorageGateway storageGateway, ISoREngine sorEngine)
 
         {
-            this.cosmosGateway = cosmosGateway;
+            this.repairStorageGateway = repairStorageGateway;
             this.storageGateway = storageGateway;
             this.sorEngine = sorEngine;
         }
@@ -54,7 +54,7 @@ namespace HousingRepairsOnlineApi.UseCases
 
             }
 
-            var savedRequest = await cosmosGateway.AddRepair(repair);
+            var savedRequest = await repairStorageGateway.AddRepair(repair);
 
             return savedRequest;
         }
