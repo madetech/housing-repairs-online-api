1b8f15f109b2c3a08ad368df0c959e8f4e4d957a
Olly Swanson <olly.swanson95@gmail.com>
2022-09-13 13:17:20 +0100

    Remove Cosmos
    In migrating the application from Azure to AWS we will be using Postgres
    as the primary datastore, we can therefore remove Cosmos specific
    code/configuration/dependencies.

--- HousingRepairsOnlineApi.Tests/UseCasesTests/SaveRepairRequestUseCaseTests.cs
+++ HousingRepairsOnlineApi.Tests/UseCasesTests/SaveRepairRequestUseCaseTests.cs
@@ -5,116 +5,91 @@ using HousingRepairsOnlineApi.UseCases;
 using Moq;
 using Xunit;
 
-namespace HousingRepairsOnlineApi.Tests.UseCasesTests
+namespace HousingRepairsOnlineApi.Tests.UseCasesTests;
+
+public class SaveRepairRequestUseCaseTests
 {
-    public class SaveRepairRequestUseCaseTests
+    private readonly Mock<IBlobStorageGateway> mockAzureStorageGateway;
+    private readonly Mock<IRepairStorageGateway> mockRepairStorageGateway;
+    private readonly Mock<ISoREngine> mockSorEngine;
+    private readonly SaveRepairRequestUseCase systemUnderTest;
+
+    public SaveRepairRequestUseCaseTests()
     {
-        private readonly SaveRepairRequestUseCase systemUnderTest;
-        private readonly Mock<ISoREngine> mockSorEngine;
-        private readonly Mock<IRepairStorageGateway> mockCosmosGateway;
-        private readonly Mock<IBlobStorageGateway> mockAzureStorageGateway;
+        mockSorEngine = new Mock<ISoREngine>();
+        mockRepairStorageGateway = new Mock<IRepairStorageGateway>();
+        mockAzureStorageGateway = new Mock<IBlobStorageGateway>();
+        systemUnderTest = new SaveRepairRequestUseCase(
+            mockRepairStorageGateway.Object,
+            mockAzureStorageGateway.Object,
+            mockSorEngine.Object
+        );
+    }
 
-        public SaveRepairRequestUseCaseTests()
+    [Fact]
+    public async void GivenARepairRequestWithImageARepairIsSaved()
+    {
+        const string Location = "kitchen";
+        const string Problem = "cupboards";
+        const string Issue = "doorHangingOff";
+        const string RepairCode = "N373049";
+        const string Base64Img =
+            "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAABhWlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw1AUhU9TpaJVB4uIOGSoThZERRy1CkWoEGqFVh1MXvojNGlIUlwcBdeCgz+LVQcXZ10dXAVB8AfE0clJ0UVKvC8ptIjxwuN9nHfP4b37AKFWYprVNgZoum2mEnExk10RQ68IIIQe9KNLZpYxK0lJ+NbXPXVT3cV4ln/fn9Wt5iwGBETiGWaYNvE68dSmbXDeJ46woqwSnxOPmnRB4keuKx6/cS64LPDMiJlOzRFHiMVCCystzIqmRjxJHFU1nfKFjMcq5y3OWqnCGvfkLwzn9OUlrtMaQgILWIQEEQoq2EAJNmK066RYSNF53Mc/6Polcink2gAjxzzK0CC7fvA/+D1bKz8x7iWF40D7i+N8DAOhXaBedZzvY8epnwDBZ+BKb/rLNWD6k/RqU4seAb3bwMV1U1P2gMsdYODJkE3ZlYK0hHweeD+jb8oCfbdA56o3t8Y5Th+ANM0qeQMcHAIjBcpe83l3R+vc/u1pzO8H+I9yds6VEEcAAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfmAQcOFjXsyx/IAAAAGXRFWHRDb21tZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAAxJREFUCNdj0HiTBAACtgF3wqeo5gAAAABJRU5ErkJggg==";
+        const string FileExtension = "png";
+        const string ImgUrl = "http://img.png";
+
+        var repairRequest = new RepairRequest
         {
-            mockSorEngine = new Mock<ISoREngine>();
-            mockCosmosGateway = new Mock<IRepairStorageGateway>();
-            mockAzureStorageGateway = new Mock<IBlobStorageGateway>();
-            systemUnderTest = new SaveRepairRequestUseCase(
-                mockCosmosGateway.Object,
-                mockAzureStorageGateway.Object,
-                mockSorEngine.Object
-                );
-        }
+            Location = new RepairLocation { Value = Location },
+            Problem = new RepairProblem { Value = Problem },
+            Issue = new RepairIssue { Value = Issue },
+            Description = new RepairDescriptionRequest { Base64Img = Base64Img, FileExtension = FileExtension }
+        };
 
-        [Fact]
-        public async void GivenARepairRequestWithImageARepairIsSaved()
+        mockSorEngine.Setup(x => x.MapSorCode(Location, Problem, Issue))
+            .Returns(RepairCode);
+
+        mockAzureStorageGateway.Setup(x => x.UploadBlob(Base64Img, FileExtension))
+            .ReturnsAsync(ImgUrl);
+
+        mockRepairStorageGateway.Setup(x => x.AddRepair(It.IsAny<Repair>()))
+            .ReturnsAsync((Repair r) => r);
+
+        var _ = await systemUnderTest.Execute(repairRequest);
+
+        mockAzureStorageGateway.Verify(x => x.UploadBlob(Base64Img, FileExtension), Times.Once);
+        mockSorEngine.Verify(x => x.MapSorCode(Location, Problem, Issue), Times.Once);
+        mockRepairStorageGateway.Verify(
+            x => x.AddRepair(It.Is<Repair>(p => p.SOR == RepairCode && p.Description.PhotoUrl == ImgUrl)), Times.Once);
+    }
+
+    [Fact]
+    public async void GivenARepairRequestWithoutAnImageARepairIsSaved()
+    {
+        const string Location = "kitchen";
+        const string Problem = "cupboards";
+        const string Issue = "doorHangingOff";
+        const string RepairCode = "N373049";
+
+        var repairRequest = new RepairRequest
         {
-            const string Location = "kitchen";
-            const string Problem = "cupboards";
-            const string Issue = "doorHangingOff";
-            const string RepairCode = "N373049";
-            const string Base64Img = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAABhWlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw1AUhU9TpaJVB4uIOGSoThZERRy1CkWoEGqFVh1MXvojNGlIUlwcBdeCgz+LVQcXZ10dXAVB8AfE0clJ0UVKvC8ptIjxwuN9nHfP4b37AKFWYprVNgZoum2mEnExk10RQ68IIIQe9KNLZpYxK0lJ+NbXPXVT3cV4ln/fn9Wt5iwGBETiGWaYNvE68dSmbXDeJ46woqwSnxOPmnRB4keuKx6/cS64LPDMiJlOzRFHiMVCCystzIqmRjxJHFU1nfKFjMcq5y3OWqnCGvfkLwzn9OUlrtMaQgILWIQEEQoq2EAJNmK066RYSNF53Mc/6Polcink2gAjxzzK0CC7fvA/+D1bKz8x7iWF40D7i+N8DAOhXaBedZzvY8epnwDBZ+BKb/rLNWD6k/RqU4seAb3bwMV1U1P2gMsdYODJkE3ZlYK0hHweeD+jb8oCfbdA56o3t8Y5Th+ANM0qeQMcHAIjBcpe83l3R+vc/u1pzO8H+I9yds6VEEcAAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfmAQcOFjXsyx/IAAAAGXRFWHRDb21tZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAAxJREFUCNdj0HiTBAACtgF3wqeo5gAAAABJRU5ErkJggg==";
-            const string FileExtension = "png";
-            const string ImgUrl = "http://img.png";
+            Location = new RepairLocation { Value = Location },
+            Problem = new RepairProblem { Value = Problem },
+            Issue = new RepairIssue { Value = Issue },
+            Description = new RepairDescriptionRequest { Text = "Lorem ipsum" }
+        };
 
-            var repairRequest = new RepairRequest()
-            {
-                Location = new RepairLocation()
-                {
-                    Value = Location
-                },
-                Problem = new RepairProblem()
-                {
-                    Value = Problem,
-                },
-                Issue = new RepairIssue()
-                {
-                    Value = Issue
-                },
-                Description = new RepairDescriptionRequest()
-                {
-                    Base64Img = Base64Img,
-                    FileExtension = FileExtension
-                }
+        mockSorEngine.Setup(x => x.MapSorCode(Location, Problem, Issue))
+            .Returns(RepairCode);
 
-            };
+        mockRepairStorageGateway.Setup(x => x.AddRepair(It.IsAny<Repair>()))
+            .ReturnsAsync((Repair r) => r);
 
-            mockSorEngine.Setup(x => x.MapSorCode(Location, Problem, Issue))
-                .Returns(RepairCode);
+        var _ = await systemUnderTest.Execute(repairRequest);
 
-            mockAzureStorageGateway.Setup(x => x.UploadBlob(Base64Img, FileExtension))
-                .ReturnsAsync(ImgUrl);
-
-            mockCosmosGateway.Setup(x => x.AddRepair(It.IsAny<Repair>()))
-                .ReturnsAsync((Repair r) => r);
-
-            var _ = await systemUnderTest.Execute(repairRequest);
-
-            mockAzureStorageGateway.Verify(x => x.UploadBlob(Base64Img, FileExtension), Times.Once);
-            mockSorEngine.Verify(x => x.MapSorCode(Location, Problem, Issue), Times.Once);
-            mockCosmosGateway.Verify(x => x.AddRepair(It.Is<Repair>(p => p.SOR == RepairCode && p.Description.PhotoUrl == ImgUrl)), Times.Once);
-        }
-
-        [Fact]
-        public async void GivenARepairRequestWithoutAnImageARepairIsSaved()
-        {
-            const string Location = "kitchen";
-            const string Problem = "cupboards";
-            const string Issue = "doorHangingOff";
-            const string RepairCode = "N373049";
-
-            var repairRequest = new RepairRequest()
-            {
-                Location = new RepairLocation()
-                {
-                    Value = Location
-                },
-                Problem = new RepairProblem()
-                {
-                    Value = Problem,
-                },
-                Issue = new RepairIssue()
-                {
-                    Value = Issue
-                },
-                Description = new RepairDescriptionRequest()
-                {
-                    Text = "Lorem ipsum"
-                }
-
-            };
-
-            mockSorEngine.Setup(x => x.MapSorCode(Location, Problem, Issue))
-                .Returns(RepairCode);
-
-            mockCosmosGateway.Setup(x => x.AddRepair(It.IsAny<Repair>()))
-                .ReturnsAsync((Repair r) => r);
-
-            var _ = await systemUnderTest.Execute(repairRequest);
-
-            mockSorEngine.Verify(x => x.MapSorCode(Location, Problem, Issue), Times.Once);
-            mockCosmosGateway.Verify(x => x.AddRepair(It.Is<Repair>(p => p.SOR == RepairCode && p.Description.PhotoUrl == null)), Times.Once);
-            mockAzureStorageGateway.Verify(x => x.UploadBlob(null, null), Times.Never());
-        }
+        mockSorEngine.Verify(x => x.MapSorCode(Location, Problem, Issue), Times.Once);
+        mockRepairStorageGateway.Verify(
+            x => x.AddRepair(It.Is<Repair>(p => p.SOR == RepairCode && p.Description.PhotoUrl == null)), Times.Once);
+        mockAzureStorageGateway.Verify(x => x.UploadBlob(null, null), Times.Never());
     }
 }
--- HousingRepairsOnlineApi/Startup.cs
+++ HousingRepairsOnlineApi/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using HousingRepairsOnline.Authentication.DependencyInjection;
 using HousingRepairsOnlineApi.Domain;
@@ -9,210 +8,181 @@ using HousingRepairsOnlineApi.Helpers;
 using HousingRepairsOnlineApi.UseCases;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Notify.Client;
 
-namespace HousingRepairsOnlineApi
+namespace HousingRepairsOnlineApi;
+
+public class Startup
 {
-    public class Startup
+    private const string HousingRepairsOnlineApiIssuerId = "Housing Repairs Online Api";
+
+    public Startup(IConfiguration configuration)
     {
-        private const string HousingRepairsOnlineApiIssuerId = "Housing Repairs Online Api";
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSoREngine("SoRConfig.json");
+
+        services.AddTransient<IRetrieveAddressesUseCase, RetrieveAddressesUseCase>();
+        services.AddTransient<IRetrieveAvailableAppointmentsUseCase, RetrieveAvailableAppointmentsUseCase>();
+        services.AddTransient<IBookAppointmentUseCase, BookAppointmentUseCase>();
+
+        var addressesApiUrl = GetEnvironmentVariable("ADDRESSES_API_URL");
+        var schedulingApiUrl = GetEnvironmentVariable("SCHEDULING_API_URL");
+        var authenticationIdentifier = GetEnvironmentVariable("AUTHENTICATION_IDENTIFIER");
+        services.AddHttpClient();
+
+        services.AddDbContext<RepairContext>(options =>
         {
-            Configuration = configuration;
-        }
+            var dbConnectionString = GetEnvironmentVariable("DB_CONNECTION_STRING");
 
-        public IConfiguration Configuration { get; }
+            options.UseNpgsql(dbConnectionString).UseSnakeCaseNamingConvention();
+        });
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddTransient<IAddressGateway, AddressGateway>(s =>
         {
-            services.AddSoREngine("SoRConfig.json");
+            var httpClient = s.GetService<HttpClient>();
+            httpClient.BaseAddress = new Uri(addressesApiUrl);
+            return new AddressGateway(httpClient, authenticationIdentifier);
+        });
 
-            services.AddTransient<IRetrieveAddressesUseCase, RetrieveAddressesUseCase>();
-            services.AddTransient<IRetrieveAvailableAppointmentsUseCase, RetrieveAvailableAppointmentsUseCase>();
-            services.AddTransient<IBookAppointmentUseCase, BookAppointmentUseCase>();
-
-            var addressesApiUrl = GetEnvironmentVariable("ADDRESSES_API_URL");
-            var schedulingApiUrl = GetEnvironmentVariable("SCHEDULING_API_URL");
-            var authenticationIdentifier = GetEnvironmentVariable("AUTHENTICATION_IDENTIFIER");
-            services.AddHttpClient();
-
-            services.AddDbContext<RepairContext>(options =>
-            {
-                var dbConnectionString = GetEnvironmentVariable("DB_CONNECTION_STRING");
-
-                options.UseNpgsql(dbConnectionString).UseSnakeCaseNamingConvention();
-            });
-
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddTransient<IAddressGateway, AddressGateway>(s =>
-            {
-                var httpClient = s.GetService<HttpClient>();
-                httpClient.BaseAddress = new Uri(addressesApiUrl);
-                return new AddressGateway(httpClient, authenticationIdentifier);
-            });
-
-            services.AddTransient<IAppointmentsGateway, AppointmentsGateway>(s =>
-            {
-                var httpClient = s.GetService<HttpClient>();
-                httpClient.BaseAddress = new Uri(schedulingApiUrl);
-                return new AppointmentsGateway(httpClient, authenticationIdentifier);
-            });
-
-            var notifyApiKey = GetEnvironmentVariable("GOV_NOTIFY_KEY");
-
-            services.AddTransient<INotifyGateway, NotifyGateway>(s =>
-                {
-                    var notifyClient = new NotificationClient(notifyApiKey);
-                    return new NotifyGateway(notifyClient);
-                }
-            );
-            var smsConfirmationTemplateId = GetEnvironmentVariable("CONFIRMATION_SMS_NOTIFY_TEMPLATE_ID");
-
-            var emailConfirmationTemplateId = GetEnvironmentVariable("CONFIRMATION_EMAIL_NOTIFY_TEMPLATE_ID");
-
-            var internalEmailConfirmationTemplateId = GetEnvironmentVariable("INTERNAL_EMAIL_NOTIFY_TEMPLATE_ID");
-
-            var internalEmail = GetEnvironmentVariable("INTERNAL_EMAIL");
-
-            var daysUntilImageExpiry = GetEnvironmentVariable("DAYS_UNTIL_IMAGE_EXPIRY");
-
-            services.AddTransient<ISendAppointmentConfirmationSmsUseCase, SendAppointmentConfirmationSmsUseCase>(s =>
-            {
-                var notifyGateway = s.GetService<INotifyGateway>();
-                return new SendAppointmentConfirmationSmsUseCase(notifyGateway, smsConfirmationTemplateId);
-            });
-
-            services.AddTransient<ISendAppointmentConfirmationEmailUseCase, SendAppointmentConfirmationEmailUseCase>(
-                s =>
-                {
-                    var notifyGateway = s.GetService<INotifyGateway>();
-                    return new SendAppointmentConfirmationEmailUseCase(notifyGateway, emailConfirmationTemplateId);
-                });
-
-            services.AddTransient<IAppointmentConfirmationSender, AppointmentConfirmationSender>();
-
-            services.AddTransient<IRetrieveImageLinkUseCase, RetrieveImageLinkUseCase>(s =>
-            {
-                var azureStorageGateway = s.GetService<IBlobStorageGateway>();
-                return new RetrieveImageLinkUseCase(azureStorageGateway, Int32.Parse(daysUntilImageExpiry));
-            });
-
-            services.AddTransient<ISendInternalEmailUseCase, SendInternalEmailUseCase>(s =>
-            {
-                var notifyGateway = s.GetService<INotifyGateway>();
-                return new SendInternalEmailUseCase(notifyGateway, internalEmailConfirmationTemplateId, internalEmail);
-            });
-
-            services.AddHousingRepairsOnlineAuthentication(HousingRepairsOnlineApiIssuerId);
-            services.AddTransient<ISaveRepairRequestUseCase, SaveRepairRequestUseCase>();
-            services.AddTransient<IInternalEmailSender, InternalEmailSender>();
-
-            // var cosmosContainer = GetCosmosContainer();
-
-            services.AddTransient<IIdGenerator, IdGenerator>();
-
-            services.AddTransient<IRepairStorageGateway, PostgresGateway>();
-
-            //var blobContainerClient = GetBlobContainerClient();
-
-            services.AddTransient<IBlobStorageGateway, AzureStorageGateway>(s =>
-            {
-                return new AzureStorageGateway(
-                    // blobContainerClient
-                );
-            });
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HousingRepairsOnlineApi", Version = "v1" });
-                c.AddJwtSecurityScheme();
-            });
-
-            var cosmosEndpointUrl = GetEnvironmentVariable("COSMOS_ENDPOINT_URL");
-            var cosmosAuthorizationKey = GetEnvironmentVariable("COSMOS_AUTHORIZATION_KEY");
-            var cosmosDatabaseId = GetEnvironmentVariable("COSMOS_DATABASE_ID");
-            var storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-            var blobContainerName = Environment.GetEnvironmentVariable("STORAGE_CONTAINER_NAME");
-
-            services.AddHealthChecks();
-            //     .AddUrlGroup(new Uri(@$"{addressesApiUrl}/health"), "Addresses API")
-            //     .AddUrlGroup(new Uri(@$"{schedulingApiUrl}/health"), "Scheduling API")
-            //     .AddCosmosDb($"AccountEndpoint={cosmosEndpointUrl};AccountKey={cosmosAuthorizationKey};", cosmosDatabaseId, name: "Azure CosmosDb")
-            //     .AddAzureBlobStorage(storageConnectionString, blobContainerName, name: "Azure Blob Storage");
-        }
-
-        private static BlobContainerClient GetBlobContainerClient()
+        services.AddTransient<IAppointmentsGateway, AppointmentsGateway>(s =>
         {
-            string storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-            string blobContainerName = Environment.GetEnvironmentVariable("STORAGE_CONTAINER_NAME");
+            var httpClient = s.GetService<HttpClient>();
+            httpClient.BaseAddress = new Uri(schedulingApiUrl);
+            return new AppointmentsGateway(httpClient, authenticationIdentifier);
+        });
 
-            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
-            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
-            return blobContainerClient;
-        }
+        var notifyApiKey = GetEnvironmentVariable("GOV_NOTIFY_KEY");
 
-        private static ContainerResponse GetCosmosContainer()
-        {
-            var endpointUrl = GetEnvironmentVariable("COSMOS_ENDPOINT_URL");
-            var authorizationKey = GetEnvironmentVariable("COSMOS_AUTHORIZATION_KEY");
-            var databaseId = GetEnvironmentVariable("COSMOS_DATABASE_ID");
-            var containerId = GetEnvironmentVariable("COSMOS_CONTAINER_ID");
-
-            CosmosClient cosmosClient = new CosmosClient(endpointUrl, authorizationKey);
-
-            Task<DatabaseResponse> databaseResponseTask = cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
-            _ = databaseResponseTask.GetAwaiter().GetResult();
-            ;
-
-            Task<ContainerResponse> cosmosContainerResponse =
-                cosmosClient.GetDatabase(databaseId).CreateContainerIfNotExistsAsync(containerId, "/RepairID");
-            ContainerResponse cosmosContainer = cosmosContainerResponse.GetAwaiter().GetResult();
-            ;
-            return cosmosContainer;
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+        services.AddTransient<INotifyGateway, NotifyGateway>(s =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HousingRepairsOnlineApi v1"));
+                var notifyClient = new NotificationClient(notifyApiKey);
+                return new NotifyGateway(notifyClient);
             }
+        );
+        var smsConfirmationTemplateId = GetEnvironmentVariable("CONFIRMATION_SMS_NOTIFY_TEMPLATE_ID");
 
-            app.UseHttpsRedirection();
+        var emailConfirmationTemplateId = GetEnvironmentVariable("CONFIRMATION_EMAIL_NOTIFY_TEMPLATE_ID");
 
-            app.UseRouting();
+        var internalEmailConfirmationTemplateId = GetEnvironmentVariable("INTERNAL_EMAIL_NOTIFY_TEMPLATE_ID");
 
-            //app.UseSentryTracing();
+        var internalEmail = GetEnvironmentVariable("INTERNAL_EMAIL");
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+        var daysUntilImageExpiry = GetEnvironmentVariable("DAYS_UNTIL_IMAGE_EXPIRY");
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHealthChecks("/health");
-                endpoints.MapControllers().RequireAuthorization();
-            });
-        }
-
-        private static string GetEnvironmentVariable(string name)
+        services.AddTransient<ISendAppointmentConfirmationSmsUseCase, SendAppointmentConfirmationSmsUseCase>(s =>
         {
-            return Environment.GetEnvironmentVariable(name) ??
-                   throw new InvalidOperationException(
-                       $"Incorrect configuration: '{name}' environment variable must be set");
+            var notifyGateway = s.GetService<INotifyGateway>();
+            return new SendAppointmentConfirmationSmsUseCase(notifyGateway, smsConfirmationTemplateId);
+        });
+
+        services.AddTransient<ISendAppointmentConfirmationEmailUseCase, SendAppointmentConfirmationEmailUseCase>(
+            s =>
+            {
+                var notifyGateway = s.GetService<INotifyGateway>();
+                return new SendAppointmentConfirmationEmailUseCase(notifyGateway, emailConfirmationTemplateId);
+            });
+
+        services.AddTransient<IAppointmentConfirmationSender, AppointmentConfirmationSender>();
+
+        services.AddTransient<IRetrieveImageLinkUseCase, RetrieveImageLinkUseCase>(s =>
+        {
+            var azureStorageGateway = s.GetService<IBlobStorageGateway>();
+            return new RetrieveImageLinkUseCase(azureStorageGateway, int.Parse(daysUntilImageExpiry));
+        });
+
+        services.AddTransient<ISendInternalEmailUseCase, SendInternalEmailUseCase>(s =>
+        {
+            var notifyGateway = s.GetService<INotifyGateway>();
+            return new SendInternalEmailUseCase(notifyGateway, internalEmailConfirmationTemplateId, internalEmail);
+        });
+
+        services.AddHousingRepairsOnlineAuthentication(HousingRepairsOnlineApiIssuerId);
+        services.AddTransient<ISaveRepairRequestUseCase, SaveRepairRequestUseCase>();
+        services.AddTransient<IInternalEmailSender, InternalEmailSender>();
+
+        services.AddTransient<IIdGenerator, IdGenerator>();
+
+        services.AddTransient<IRepairStorageGateway, PostgresGateway>();
+
+        //var blobContainerClient = GetBlobContainerClient();
+
+        services.AddTransient<IBlobStorageGateway, AzureStorageGateway>(s =>
+        {
+            return new AzureStorageGateway(
+                // blobContainerClient
+            );
+        });
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "HousingRepairsOnlineApi", Version = "v1" });
+            c.AddJwtSecurityScheme();
+        });
+
+        var storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+        var blobContainerName = Environment.GetEnvironmentVariable("STORAGE_CONTAINER_NAME");
+
+        services.AddHealthChecks();
+        //     .AddUrlGroup(new Uri(@$"{addressesApiUrl}/health"), "Addresses API")
+        //     .AddUrlGroup(new Uri(@$"{schedulingApiUrl}/health"), "Scheduling API")
+        //     .AddAzureBlobStorage(storageConnectionString, blobContainerName, name: "Azure Blob Storage");
+    }
+
+    private static BlobContainerClient GetBlobContainerClient()
+    {
+        var storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+        var blobContainerName = Environment.GetEnvironmentVariable("STORAGE_CONTAINER_NAME");
+
+        var blobServiceClient = new BlobServiceClient(storageConnectionString);
+        var blobContainerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+        return blobContainerClient;
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HousingRepairsOnlineApi v1"));
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        //app.UseSentryTracing();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHealthChecks("/health");
+            endpoints.MapControllers().RequireAuthorization();
+        });
+    }
+
+    private static string GetEnvironmentVariable(string name)
+    {
+        return Environment.GetEnvironmentVariable(name) ??
+               throw new InvalidOperationException(
+                   $"Incorrect configuration: '{name}' environment variable must be set");
     }
 }
