245ae46d6fe24260420aba55c91ebe65a8372ab2
Olly Swanson <olly.swanson95@gmail.com>
2022-10-05 11:22:02 +0100

    Use C# annotation instead of Jetbrains

--- HousingRepairsOnlineApi/Gateways/IAppointmentsGateway.cs
+++ HousingRepairsOnlineApi/Gateways/IAppointmentsGateway.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using HACT.Dtos;
 using HousingRepairsOnlineApi.Domain;
-using JetBrains.Annotations;
 
 namespace HousingRepairsOnlineApi.Gateways;
 
