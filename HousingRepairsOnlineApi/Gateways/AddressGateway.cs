cc33d2aeddc8bdfce4536b8426736c0010409756
GeorgianaNicolae <georgiana.nicolae@madetech.com>
2022-10-11 12:39:15 +0100

    Sanitised postcode
    The buildings register api required postcodes of the form "E112LE"
    i.e no white space and uppercase

--- HousingRepairsOnlineApi/Gateways/AddressGateway.cs
+++ HousingRepairsOnlineApi/Gateways/AddressGateway.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
@@ -21,10 +23,15 @@ public class AddressGateway : IAddressGateway
 
     public async Task<IEnumerable<Address>> Search(string postcode)
     {
-        var response = await _addressesApiUrl.AppendPathSegment("/addresses").SetQueryParam("postcode", postcode)
+        var sanitisedPostcode = postcode
+            .Replace(" ", string.Empty,StringComparison.InvariantCultureIgnoreCase)
+            .ToUpper(CultureInfo.InvariantCulture);
+
+        var response = await _addressesApiUrl.AppendPathSegment("/addresses").SetQueryParam("postcode", sanitisedPostcode)
             .WithHeader("organisation_id", _addressesOrganisationId)
             .GetAsync()
             .ReceiveJson<List<BuildingsRegisterAddress>>();
+
 
         return response.ToAddresses();
     }
