67f7d626080cb2e28026024059ce6d65e0439996
GeorgianaNicolae <georgiana.nicolae@madetech.com>
2022-10-13 11:17:07 +0100

    Create logs for Appointments Controller
    We've replaced the sentry logging with the
    built in c# logger

--- HousingRepairsOnlineApi.Tests/ControllersTests/AddressesControllerTests.cs
+++ HousingRepairsOnlineApi.Tests/ControllersTests/AddressesControllerTests.cs
@@ -1,43 +1,46 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using HousingRepairsOnlineApi.Controllers;
 using HousingRepairsOnlineApi.UseCases;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Xunit;
 
-namespace HousingRepairsOnlineApi.Tests
+namespace HousingRepairsOnlineApi.Tests;
+
+public class AddressesControllerTests : ControllerTests
 {
-    public class AddressesControllerTests : ControllerTests
+    private readonly Mock<IRetrieveAddressesUseCase> retrieveAddressesUseCaseMock;
+    private readonly AddressesController systemUnderTest;
+
+    public AddressesControllerTests()
     {
-        private AddressesController systemUnderTest;
-        private Mock<IRetrieveAddressesUseCase> retrieveAddressesUseCaseMock;
+        retrieveAddressesUseCaseMock = new Mock<IRetrieveAddressesUseCase>();
+        systemUnderTest = new AddressesController(
+            retrieveAddressesUseCaseMock.Object,
+            NullLogger<AddressesController>.Instance);
+    }
 
-        public AddressesControllerTests()
-        {
-            retrieveAddressesUseCaseMock = new Mock<IRetrieveAddressesUseCase>();
-            systemUnderTest = new AddressesController(retrieveAddressesUseCaseMock.Object);
-        }
+    [Fact]
+    public async Task TestEndpoint()
+    {
+        const string Postcode = "M3 0W";
+        var result = await systemUnderTest.Addresses(Postcode);
 
-        [Fact]
-        public async Task TestEndpoint()
-        {
-            const string Postcode = "M3 0W";
-            var result = await systemUnderTest.Addresses(Postcode);
+        GetStatusCode(result).Should().Be(200);
+        retrieveAddressesUseCaseMock.Verify(x => x.Execute(Postcode), Times.Once);
+    }
 
-            GetStatusCode(result).Should().Be(200);
-            retrieveAddressesUseCaseMock.Verify(x => x.Execute(Postcode), Times.Once);
-        }
+    [Fact]
+    public async Task ReturnsErrorWhenFailsToSave()
+    {
+        const string Postcode = "M3 0W";
 
-        [Fact]
-        public async Task ReturnsErrorWhenFailsToSave()
-        {
-            const string Postcode = "M3 0W";
+        retrieveAddressesUseCaseMock.Setup(x => x.Execute(It.IsAny<string>())).Throws<Exception>();
 
-            retrieveAddressesUseCaseMock.Setup(x => x.Execute(It.IsAny<string>())).Throws<System.Exception>();
+        var result = await systemUnderTest.Addresses(Postcode);
 
-            var result = await systemUnderTest.Addresses(Postcode);
-
-            GetStatusCode(result).Should().Be(500);
-        }
+        GetStatusCode(result).Should().Be(500);
     }
 }
